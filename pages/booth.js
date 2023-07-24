d9641a50d4fcd7fb7bf297e787ae16874bfa20c1
Allen Helton <allenheltondev@gmail.com>
2023-07-24 14:44:14 -0500

    Updates for racing

--- pages/booth.js
+++ pages/booth.js
@@ -2,23 +2,30 @@ import React, { useEffect, useState, useRef } from 'react';
 import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { TopicClient, CacheClient, CredentialProvider, Configurations, CacheSortedSetFetch, CacheDictionaryFetch } from '@gomomento/sdk-web';
-import { Flex, Table, TableCell, TableHead, TableRow, TableBody, ThemeProvider, Theme } from '@aws-amplify/ui-react';
+import { Flex, Table, TableCell, TableHead, TableRow, TableBody, ThemeProvider, Theme, Heading, Divider, Card, Image } from '@aws-amplify/ui-react';
 import { getAuthToken } from '../utils/Auth';
+import { getUserDetail } from '../utils/Device';
 import { toast } from 'react-toastify';
+import { FaPlayCircle, FaRegStopCircle } from 'react-icons/fa';
+import { VscDebugRestart } from 'react-icons/vsc';
 
 const BoothPage = () => {
   const router = useRouter();
   const [cacheClient, setCacheClient] = useState(null);
   const [topicClient, setTopicClient] = useState(null);
   const [leaderboard, setLeaderboard] = useState([]);
+  const [isRaceActive, setIsRaceActive] = useState(false);
+  const [raceMessage, setRaceMessage] = useState('');
   const cacheClientRef = useRef(cacheClient);
+  const [racers, setRacers] = useState({ superMo: 0, fauxMo: 0, ko: 0 });
+  const racerRef = useRef(racers);
 
   useEffect(() => {
     async function initialize() {
       const token = await getAuthToken();
 
       const client = new CacheClient({
-        configuration: Configurations.Browser.latest(),
+        configuration: Configurations.Laptop.latest(),
         credentialProvider: CredentialProvider.fromString({ authToken: token }),
         defaultTtlSeconds: 43200 // 12 hours
       });
@@ -26,7 +33,7 @@ const BoothPage = () => {
       updateCacheClient(client);
 
       topicClient = new TopicClient({
-        configuration: Configurations.Browser.latest(),
+        configuration: Configurations.Laptop.latest(),
         credentialProvider: CredentialProvider.fromString({ authToken: token })
       });
 
@@ -40,7 +47,7 @@ const BoothPage = () => {
       });
 
       topicClient.subscribe('conference', 'racer', {
-        onItem: (item) => { updateRacer(JSON.parse(item.value())) },
+        onItem: (item) => { updateRacer(item.value()) },
         onError: (e) => {
           console.error(e.errorCode(), e.message());
           toast.error('Failed to get race data', { position: 'top-right', autoClose: 10000, draggable: false, hideProgressBar: true, theme: 'colored' });
@@ -48,7 +55,12 @@ const BoothPage = () => {
       });
     }
 
-    initialize();
+    const user = getUserDetail();
+    if (!user) {
+      router.push('/profile?redirect=/booth');
+    } else {
+      initialize();
+    }
   }, []);
 
   useEffect(() => {
@@ -99,37 +111,95 @@ const BoothPage = () => {
     setLeaderboard(board);
   };
 
-  const updateRacer = (racerData) => {
-    if (!isRacing) return;
+  const updateRacer = (racer) => {
+    if (!isRaceActive) return;
 
-    
+    if (['superMo', 'fauxMo', 'ko'].includes(racer)) {
+      const newRacers = { ...racers, [racer]: racers[racer] += 1 };
+      setRacers(newRacers);
+      racerRef.current = newRacers;
+
+      let isRaceOver = false;
+      let winner;
+      if(newRacers.superMo >= 100){
+        isRaceOver = true;
+        winner = 'Mo';        
+      }
+    }
   }
+
+  const toggleRace = async (isActive) => {
+    setIsRaceActive(isActive);
+    await cacheClientRef.current.set('conference', 'race', `${isActive}`);
+    await topicClient.publish('conference', 'race', `${isActive}`);
+  }
+
+  const restartRace = async () => {
+    toggleRace(false);
+
+    const resetRacers = { superMo: 0, fauxMo: 0, ko: 0 };
+    setRacers(resetRacers);
+    racerRef.current = resetRacers;
+  };
 
   return (
     <>
       <ThemeProvider theme={theme}>
         <Head>
-          <title>{router.query.code} Check Code | Momento</title>
+          <title>Momento Booth</title>
         </Head>
         <Flex direction="column" width="100%" alignItems="center" justifyContent="center">
-          <Table title="Scavenger Hunt Leaderboard" variation="striped" highlightOnHover width="90%" boxShadow="medium" backgroundColor="#AEE2B3">
-            <TableHead>
-              <TableRow backgroundColor="white">
-                <TableCell as="th">Rank</TableCell>
-                <TableCell as="th" colSpan={3}>Username</TableCell>
-                <TableCell as="th">Score</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {leaderboard?.map(entry => (
-                <TableRow key={entry.username}>
-                  <TableCell>{entry.rank}</TableCell>
-                  <TableCell colSpan={3}>{entry.username}</TableCell>
-                  <TableCell>{entry.score}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+          <Flex direction="row" alignContent="space-between" width="100%" padding="0em 2em" height="92vh">
+            <Flex direction="column" gap="1em" basis="48%" height="min-content">
+              <Card variation="elevated" backgroundColor="#C4F135">
+                <Heading level={4} textAlign="center">Scavenger Hunt Leaderboard</Heading>
+                <FaRegStopCircle size="1.5em" cursor="pointer" onClick={() => updateRacer('superMo')} />
+              </Card>
+              <Table title="Scavenger Hunt Leaderboard" variation="striped" highlightOnHover boxShadow="medium" backgroundColor="#AEE2B3" >
+                <TableHead>
+                  <TableRow backgroundColor="white">
+                    <TableCell as="th">Rank</TableCell>
+                    <TableCell as="th" colSpan={3}>Username</TableCell>
+                    <TableCell as="th">Score</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {leaderboard?.map(entry => (
+                    <TableRow key={entry.username}>
+                      <TableCell>{entry.rank}</TableCell>
+                      <TableCell colSpan={3}>{entry.username}</TableCell>
+                      <TableCell>{entry.score}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </Flex>
+            <Divider orientation="vertical" size="large" />
+            <Flex direction="column" basis="48%">
+              <Card variation="elevated" backgroundColor="#C4F135">
+                <Flex direction="row" alignItems="center" justifyContent="space-between">
+                  <VscDebugRestart size="1.5em" cursor="pointer" onClick={restartRace} />
+                  <Heading level={4} textAlign="center" >Momento Racers</Heading>
+                  {isRaceActive ? <FaRegStopCircle size="1.5em" cursor="pointer" onClick={() => toggleRace(false)} /> : <FaPlayCircle size="1.5em" cursor="pointer" onClick={() => toggleRace(true)} />}
+                </Flex>
+              </Card>
+              <Card variation="elevated" width="100%" backgroundColor="#C2B2A9">
+                <Flex direction="row" gap=".5em" justifyContent="center">
+                  <Divider orientation="vertical" size="small" basis="2%" />
+                  <Flex direction="column" gap="1em" basis="96%">
+                    <Divider size="small" />
+                    <Image key="superMo" src="mo.png" left={`${racerRef.current.superMo}%`} width="4em" position="relative" />
+                    <Divider size="small" />
+                    <Image key="fauxMo" src="fauxmo.png" left={`${racerRef.current.fauxMo}%`} width="4em" position="relative" />
+                    <Divider size="small" />
+                    <Image key="ko" src="ko.png" left={`${racerRef.current.ko}%`} width="3em" position="relative" />
+                    <Divider size="small" />
+                  </Flex>
+                  <Divider orientation="vertical" size="small" basis="2%" />
+                </Flex>
+              </Card>
+            </Flex>
+          </Flex>
         </Flex>
       </ThemeProvider>
     </>
---
+++ pages/racer.js
@@ -0,0 +1,86 @@
+import { useEffect, useState } from 'react';
+import { Flex, Card, Heading, Button, Text, Image } from '@aws-amplify/ui-react';
+import Head from 'next/head';
+import { TopicClient, CacheClient, CredentialProvider, Configurations, CacheGet } from '@gomomento/sdk-web';
+import { getAuthToken } from '../utils/Auth';
+import { toast } from 'react-toastify';
+
+const RacerPage = () => {
+  const [topicClient, setTopicClient] = useState(null);
+  const [isRaceActive, setIsRaceActive] = useState(false);
+
+  useEffect(() => {
+    const setupTopicClient = async () => {
+      const authToken = await getAuthToken();
+      topicClient = new TopicClient({
+        configuration: Configurations.Browser.latest(),
+        credentialProvider: CredentialProvider.fromString({ authToken })
+      });
+
+      setTopicClient(topicClient);
+
+      const cacheClient = new CacheClient({
+        configuration: Configurations.Browser.latest(),
+        credentialProvider: CredentialProvider.fromString({ authToken }),
+        defaultTtlSeconds: 60
+      });
+
+      const raceStatusResponse = await cacheClient.get('conference', 'race');
+      if (raceStatusResponse instanceof CacheGet.Hit) {
+        setIsRaceActive(raceStatusResponse.valueString() == "true");
+      }
+    };
+
+    setupTopicClient();
+  }, []);
+
+  useEffect(() => {
+    const subscribeForUpdates = async () => {
+      await topicClient.subscribe('conference', 'race', {
+        onItem: (data) => { setIsRaceActive(data.value() == 'true') },
+        onError: (err) => {
+          console.error(e.errorCode(), e.message());
+          toast.error('Failed to get race details', { position: 'top-right', autoClose: 10000, draggable: false, hideProgressBar: true, theme: 'colored' });
+        }
+      });
+    };
+
+    if (topicClient) {
+      subscribeForUpdates();
+    }
+  }, [topicClient]);
+
+  const moveRacer = async (racer) => {
+    await topicClient.publish('conference', 'racer', racer);
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Race | Momento</title>
+      </Head>
+      <Flex direction="column" width="100%" alignItems="center" marginTop="1em">
+        <Card variation="elevated" borderRadius="large" padding="1.5em 3em" width="90%">
+          <Flex direction="column" gap="1em" alignItems="center">
+            <Heading level={4}>{isRaceActive ? 'Press your racer as fast as you can!' : 'Waiting for race to start'}</Heading>
+            <Text fontSize=".9rem" textAlign="center"><i>{!isRaceActive ? 'When the race starts, press the button for your racer as fast as you can!' : 'Now is your chance, press the button! Go! Go! Go!'}</i></Text>
+            <Flex direction="row" gap="1em" wrap="wrap" alignItems="center" justifyContent="center">
+              <Button isDisabled={!isRaceActive} variation="link" name="superMo" onClick={(e) => moveRacer(e.target.name)} >
+                <Image name="superMo" src="/mo-profile.png" width="10em" borderRadius="50%" boxShadow="large" />
+              </Button>
+              <Button isDisabled={!isRaceActive} variation="link" name="fauxMo" onClick={(e) => moveRacer(e.target.name)} >
+                <Image name="fauxMo" src="/faux-mo-profile.png" width="10em" borderRadius="50%" boxShadow="large"/>
+              </Button>
+              <Button isDisabled={!isRaceActive} variation="link" name="ko" onClick={(e) => moveRacer(e.target.name)} >
+                <Image name="ko" src="/ko-profile.png" width="10em" borderRadius="50%" boxShadow="large"/>
+              </Button>
+            </Flex>
+          </Flex>
+        </Card>
+      </Flex>
+    </>
+  );
+};
+
+export default RacerPage;
+
